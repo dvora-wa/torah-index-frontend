25ad8ddf8f922534808eba93256b89bd157740af
dvora-wa <dw4121060@gmail.com>
2026-01-01 20:52:41 +0200

    chore: adjust angular.json production settings

--- src/app/services/index/index.service.ts
+++ src/app/services/index/index.service.ts
@@ -1,33 +1,27 @@
-// import { Injectable } from '@angular/core';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class Index {
-
-// }
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IndexEntry, IndexResponse, IndexType } from '../../models';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IndexService {
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
+  
 
   generateIndex(file: File, type: IndexType): Observable<IndexResponse> {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('indexType', type.toString());
 
-    return this.http.post<IndexResponse>('http://localhost:3000/api/index/generate', formData);
+    return this.http.post<IndexResponse>(`${this.apiUrl}/index/generate`, formData);
   }
 
   exportToWord(entries: IndexEntry[], type: IndexType): Observable<Blob> {
-    return this.http.post('http://localhost:3000/api/index/export-word', { entries, type }, { responseType: 'blob' });
+    return this.http.post(`${this.apiUrl}/index/export-word`, { entries, type }, { responseType: 'blob' });
   }
 }
